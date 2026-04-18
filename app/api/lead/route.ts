dd67acbe84b33aca686af526648bebf588fa1a25
Vishwa <vishwasrivats@gmail.com>
2026-04-18 13:49:21 +0530

    Initial folder structure

---
+++ app/api/lead/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    console.log("Lead received:", body);
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json({ success: false });
+  }
+}
